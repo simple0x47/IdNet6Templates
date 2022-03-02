84026eee2a5f7887457123313dee6370a01f5243
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2022-03-02 20:27:13 +0100

    add: initial state

---
+++ README.md
@@ -0,0 +1,39 @@
+# IdNet6.Templates
+dotnet new templates for IdNet6
+
+## Important
+This organization is not maintained anymore besides critical security bugfixes (if feasible). This organization will be archived when .NET Core 3.1 end of support is reached (3rd Dec 2022). All new development is happening in the new [Duende Software](https://github.com/duendesoftware) organization. 
+
+The new [Duende IdentityServer](https://duendesoftware.com/products/identityserver) comes with a commercial license but is [free](https://blog.duendesoftware.com/posts/20220111_fair_trade/) for dev/testing/personal projects and companies or individuals making less than 1M USD gross annnual revenue. Please [get in touch with us](https://duendesoftware.com/contact) if you have any question.
+
+### dotnet new is4empty
+Creates a minimal IdNet6 project without a UI.
+
+### dotnet new is4ui
+Adds the quickstart UI to the current project (can be e.g added on top of *is4empty*)
+
+### dotnet new is4inmem
+Adds a basic IdentityServer with UI, test users and sample clients and resources. Shows both in-memory code and JSON configuration.
+
+### dotnet new is4aspid
+Adds a basic IdentityServer that uses ASP.NET Identity for user management. If you automatically seed the database, you will get two users: `alice` and `bob` - both with password `Pass123$`. Check the `SeedData.cs` file.
+
+### dotnet new is4ef
+Adds a basic IdentityServer that uses Entity Framework for configuration and state management. If you seed the database, you get a couple of basic client and resource registrations, check the `SeedData.cs` file.
+
+### dotnet new is4admin
+Adds an IdentityServer that includes the Rock Solid Knowledge AdminUI Community Edition (open `http://localhost:5000/admin` in the browser). This gives you a web-based administration interface for users, claims, clients and resources.
+
+The community edition is intended for testing IdentityServer integration scenarios and is limited to localhost:5000, SQLite, 10 users, and 2 clients. *The community edition is not suitable for production*.
+
+See [identityserver.com](https://www.identityserver.com/products) for more information about AdminUI or to request a trial license.
+
+## Installation 
+
+Install with:
+
+`dotnet new -i IdNet6.templates`
+
+If you need to set back your dotnet new list to "factory defaults", use this command:
+
+`dotnet new --debug:reinit`
---
+++ build.ps1
@@ -0,0 +1,102 @@
+$CakeVersion = "0.30.0"
+
+$TOOLS_DIR = Join-Path $PSScriptRoot "tools"
+$NUGET_EXE = Join-Path $TOOLS_DIR "nuget.exe"
+$NUGET_URL = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+
+# Make sure tools folder exists
+$PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
+$ToolPath = Join-Path $PSScriptRoot "tools"
+if (!(Test-Path $ToolPath)) {
+    Write-Verbose "Creating tools directory..."
+    New-Item -Path $ToolPath -Type directory | out-null
+}
+
+# Attempt to set highest encryption available for SecurityProtocol.
+# PowerShell will not set this by default (until maybe .NET 4.6.x). This
+# will typically produce a message for PowerShell v2 (just an info
+# message though)
+try {
+    # Set TLS 1.2 (3072), then TLS 1.1 (768), then TLS 1.0 (192), finally SSL 3.0 (48)
+    # Use integers because the enumeration values for TLS 1.2 and TLS 1.1 won't
+    # exist in .NET 4.0, even though they are addressable if .NET 4.5+ is
+    # installed (.NET 4.5 is an in-place upgrade).
+    [System.Net.ServicePointManager]::SecurityProtocol = 3072 -bor 768 -bor 192 -bor 48
+  } catch {
+    Write-Output 'Unable to set PowerShell to use TLS 1.2 and TLS 1.1 due to old .NET Framework installed. If you see underlying connection closed or trust errors, you may need to upgrade to .NET Framework 4.5+ and PowerShell v3'
+  }
+
+###########################################################################
+# INSTALL NUGET
+###########################################################################
+
+# Try find NuGet.exe in path if not exists
+if (!(Test-Path $NUGET_EXE)) {
+    Write-Verbose -Message "Trying to find nuget.exe in PATH..."
+    $existingPaths = $Env:Path -Split ';' | Where-Object { (![string]::IsNullOrEmpty($_)) -and (Test-Path $_ -PathType Container) }
+    $NUGET_EXE_IN_PATH = Get-ChildItem -Path $existingPaths -Filter "nuget.exe" | Select -First 1
+    if ($NUGET_EXE_IN_PATH -ne $null -and (Test-Path $NUGET_EXE_IN_PATH.FullName)) {
+        Write-Verbose -Message "Found in PATH at $($NUGET_EXE_IN_PATH.FullName)."
+        $NUGET_EXE = $NUGET_EXE_IN_PATH.FullName
+    }
+}
+
+# Try download NuGet.exe if not exists
+if (!(Test-Path $NUGET_EXE)) {
+    Write-Verbose -Message "Downloading NuGet.exe..."
+    try {
+        (New-Object System.Net.WebClient).DownloadFile($NUGET_URL, $NUGET_EXE)
+    } catch {
+        Throw "Could not download NuGet.exe."
+    }
+}
+
+# Save nuget.exe path to environment to be available to child processed
+$ENV:NUGET_EXE = $NUGET_EXE
+
+###########################################################################
+# INSTALL CAKE
+###########################################################################
+
+# Make sure Cake has been installed.
+$CakePath = Join-Path $ToolPath ".store\cake.tool\$CakeVersion"
+$CakeExePath = (Get-ChildItem -Path $ToolPath -Filter "dotnet-cake*" -File| ForEach-Object FullName | Select-Object -First 1)
+
+if ((!(Test-Path -Path $CakePath -PathType Container)) -or (!(Test-Path $CakeExePath -PathType Leaf))) {
+    & dotnet tool install --tool-path $ToolPath --version $CakeVersion Cake.Tool
+    if ($LASTEXITCODE -ne 0)
+    {
+        'Failed to install cake'
+        exit 1
+    }
+    $CakeExePath = (Get-ChildItem -Path $ToolPath -Filter "dotnet-cake*" -File| ForEach-Object FullName | Select-Object -First 1)
+}
+
+###########################################################################
+# RUN BUILD SCRIPT
+###########################################################################
+Write-Host "build template code..."
+
+& "$CakeExePath" ./build.cake --bootstrap
+if ($LASTEXITCODE -eq 0)
+{
+    & "$CakeExePath" ./build.cake -Target="Build"
+}
+
+Write-Host "clean..."
+Invoke-Expression "git clean -xdf ./src"
+Invoke-Expression "git clean -xdf ./feed"
+Invoke-Expression "git clean -xdf ./UI"
+
+Write-Host "Downloading quickstart UI..."
+cd .\UI
+iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/IdentityServer/IdNet6.Quickstart.UI/main/getmain.ps1'))
+cd ..
+
+dotnet tool restore
+& "$CakeExePath" ./build.cake --bootstrap
+if ($LASTEXITCODE -eq 0)
+{
+    & "$CakeExePath" ./build.cake $args
+}
+exit $LASTEXITCODE
